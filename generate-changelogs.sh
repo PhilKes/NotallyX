a3281d8195a55586c54924c054197bc9681fbc5d
PhilKes <phil.kessler@gmx.de>
2025-03-02 12:18:58 +0100

    Exclude issues labeled 'already done' from CHANGELOG.md

--- generate-changelogs.sh
+++ generate-changelogs.sh
@@ -3,4 +3,4 @@
 VERSION_NAME=$1
 OUTPUT_FILE=$2
 echo "Generating CHANGELOG.md entry since $VERSION_NAME to $OUTPUT_FILE"
-github_changelog_generator -u PhilKes -p NotallyX --since-tag $VERSION_NAME --no-pull-requests --include-tags-regex '^v\d+(\.\d+)*$' --include-labels enhancement,bug --enhancement-label "### Added Features" --bugs-label "### Fixed Bugs" --base $OUTPUT_FILE --output $OUTPUT_FILE
+github_changelog_generator -u PhilKes -p NotallyX --since-tag $VERSION_NAME --no-pull-requests --include-tags-regex '^v\d+(\.\d+)*$' --include-labels enhancement,bug --exclude-labels duplicate,question,invalid,wontfix,'already done' --enhancement-label "### Added Features" --bugs-label "### Fixed Bugs" --base $OUTPUT_FILE --output $OUTPUT_FILE
